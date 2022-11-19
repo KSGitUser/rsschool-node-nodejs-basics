cd8d9ec5dfb6d7bd5df33df6d5c52840f2748f00
Sergey Krikun <sergey.a.krikun@gmail.com>
2022-11-19 19:29:06 +0300

    feature: add copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,24 @@
+import { copyFile , constants, readdir, mkdir } from 'node:fs/promises'
+import { join, dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
 const copy = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url))
+    const src = join(__dirname, '/files')
+    const dist = join(__dirname, '/files_copy')
+
+    try {
+        const files = await readdir(src)
+        await mkdir(dist)
+        for (const file of files) {
+            await copyFile(join(src,file), join(dist,file), constants.COPYFILE_EXCL)
+        }
+    } catch (err) {
+        if (err.code === 'EEXIST') {
+            throw new Error('FS operation failed')
+        }
+        throw err;
+    }
 };
 
 copy();
