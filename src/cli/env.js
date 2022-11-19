aa50b3dbee17720caefadc3f0f8a27f436136e55
Sergey Krikun <sergey.a.krikun@gmail.com>
2022-11-19 23:54:40 +0300

    feat: env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
+import process from "node:process";
+
 const parseEnv = () => {
-    // Write your code here 
+  const envs = process.env; // Write your code here
+  const foundEnvs = [];
+  Reflect.ownKeys(envs)
+    .filter((key) => key.startsWith("RSS_"))
+    .forEach((key) => {
+      foundEnvs.push(`${key}=${envs[key]}`);
+    });
+  console.log(foundEnvs.join("; "));
 };
 
 parseEnv();
