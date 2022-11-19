8faa3df37c883351530372f6a285e5786b4e4a45
Sergey Krikun <sergey.a.krikun@gmail.com>
2022-11-19 21:58:57 +0300

    feat: delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
+import { rm } from "node:fs/promises";
+import { join } from "node:path";
+import getDirname from "../utils/getDirname.js";
+
 const remove = async () => {
-    // Write your code here 
+  const fileName = "fileToRemove.txt";
+  const pathToFile = "/files";
+  const currentDirectory = getDirname(import.meta.url);
+
+  try {
+    await rm(join(currentDirectory, pathToFile, fileName));
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      throw new Error("FS operation failed");
+    }
+    throw err;
+  }
 };
 
 await remove();
