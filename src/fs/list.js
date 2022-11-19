a7e6561fdbb950f4bca24471a37926b11b820e5b
Sergey Krikun <sergey.a.krikun@gmail.com>
2022-11-19 22:05:32 +0300

    feat: list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
+import { readdir } from "node:fs/promises";
+import { join } from "node:path";
+import getDirname from "../utils/getDirname.js";
+
 const list = async () => {
-    // Write your code here 
+  const currentDirectoryPath = getDirname(import.meta.url);
+  const pathToDir = join(currentDirectoryPath, "/files");
+
+  try {
+    const files = await readdir(pathToDir);
+    console.log(files);
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      throw new Error("FS operation failed");
+    }
+    throw err;
+  }
 };
 
 await list();
