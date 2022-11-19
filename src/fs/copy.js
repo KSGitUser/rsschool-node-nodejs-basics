ed0a183e1568157efc5b8ac89379e9bd7d9fb297
Sergey Krikun <sergey.a.krikun@gmail.com>
2022-11-19 20:10:56 +0300

    fix: format code

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,24 +1,28 @@
-import { copyFile , constants, readdir, mkdir } from 'node:fs/promises'
-import { join, dirname } from 'node:path'
-import { fileURLToPath } from 'node:url'
+import { copyFile, constants, readdir, mkdir } from "node:fs/promises";
+import { join, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
 
 const copy = async () => {
-    const __dirname = dirname(fileURLToPath(import.meta.url))
-    const src = join(__dirname, '/files')
-    const dist = join(__dirname, '/files_copy')
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const src = join(__dirname, "/files");
+  const dist = join(__dirname, "/files_copy");
 
-    try {
-        const files = await readdir(src)
-        await mkdir(dist)
-        for (const file of files) {
-            await copyFile(join(src,file), join(dist,file), constants.COPYFILE_EXCL)
-        }
-    } catch (err) {
-        if (err.code === 'EEXIST') {
-            throw new Error('FS operation failed')
-        }
-        throw err;
+  try {
+    const files = await readdir(src);
+    await mkdir(dist);
+    for (const file of files) {
+      await copyFile(
+        join(src, file),
+        join(dist, file),
+        constants.COPYFILE_EXCL
+      );
     }
+  } catch (err) {
+    if (err.code === "EEXIST") {
+      throw new Error("FS operation failed");
+    }
+    throw err;
+  }
 };
 
 copy();
