dbb3861393e4405eaf4642e4fd1cfcd9c189824e
Sergey Krikun <sergey.a.krikun@gmail.com>
2022-11-20 17:11:51 +0300

    feat: write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
+import getDirname from "../utils/getDirname.js";
+import { join } from "node:path";
+import { stdin } from "node:process";
+import { createWriteStream } from "node:fs";
+
 const write = async () => {
-    // Write your code here 
+  const pathToFile = "./files/fileToWrite.txt";
+  const fullPathToFile = join(getDirname(import.meta.url), pathToFile);
+  const ws = createWriteStream(fullPathToFile, { encoding: "utf8" });
+
+  stdin.pipe(ws);
 };
 
 await write();
