5ffed6bb3931bdbc043517229dfc67566c756c51
Sergey Krikun <sergey.a.krikun@gmail.com>
2022-11-25 00:20:39 +0300

    feat: add cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,18 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { spawn } from "node:child_process";
+import getDirname from "../utils/getDirname.js";
+import { join } from "node:path";
+
+const spawnChildProcess = async (...args) => {
+  const pathToChildProcess = "./files/script.js";
+  const currentDirname = getDirname(import.meta.url);
+  const fullPathToChildProcess = join(currentDirname, pathToChildProcess);
+  const childProcess = spawn(process.execPath, [
+    fullPathToChildProcess,
+    ...args,
+  ]);
+
+  process.stdin.pipe(childProcess.stdin);
+  childProcess.stdout.pipe(process.stdout);
 };
 
 spawnChildProcess();
